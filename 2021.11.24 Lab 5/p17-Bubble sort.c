28e2c76a67fbb10ac3b61f66c8d829ccc2de3194
Vrishabh-D-B <bansodvrishabh@pm.me>
2021-11-27 23:34:53 +0530

    lab

--- 2021.11.24 Lab 5/p17-Bubble sort.c
+++ 2021.11.24 Lab 5/p17-Bubble sort.c
@@ -24,9 +24,13 @@ int main()
 {
 	int arr[] = {64, 34, 25, 12, 22, 11, 90};
 	int n = sizeof(arr)/sizeof(arr[0]);
+	int i;
+	printf("Before Sort : \n");
+	for (i=0; i < n; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
 	bubbleSort(arr, n);
 	printf("Sorted array: \n");
-	int i;
 	for (i=0; i < n; i++)
 		printf("%d ", arr[i]);
 	printf("\n");
