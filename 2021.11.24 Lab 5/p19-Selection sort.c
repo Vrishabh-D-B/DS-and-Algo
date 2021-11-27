9d655984ec9094d0d38fd816102928bfa5bdbcc5
Vrishabh-D-B <bansodvrishabh@pm.me>
2021-11-27 23:32:09 +0530

    lab

--- 2021.11.24 Lab 5/p19-Selection sort.c
+++ 2021.11.24 Lab 5/p19-Selection sort.c
@@ -27,9 +27,14 @@ int main()
 {
 	int arr[] = {64, 25, 12, 22, 11};
 	int n = sizeof(arr)/sizeof(arr[0]);
+	int i;
+	printf("Before Sort : \n");
+	for (i=0; i < n; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
 	selectionSort(arr, n);
 	printf("Sorted array: \n");
-    int i;
+    
 	for (i=0; i < n; i++)
 		printf("%d ", arr[i]);
 	printf("\n");
