a61608fde19b1089a88679302cc6069d7c213af3
Vrishabh-D-B <bansodvrishabh@pm.me>
2021-11-28 14:54:44 +0530

    lab

--- 2021.10.27 Lab 2/p3-Array sum.c
+++ 2021.10.27 Lab 2/p3-Array sum.c
@@ -5,18 +5,18 @@ void main()
 {
     int i, arr1[NUM], arr2[NUM], arr3[NUM];
     
-    printf("\nEnter %d values for arr1 : ",NUM);
+    printf("Enter %d values for arr1 : ",NUM);
     for(i=0;i<NUM;i++)
         scanf("%d",&arr1[i]);
 
-    printf("\nEnter %d values for arr2 : ",NUM);
+    printf("Enter %d values for arr2 : ",NUM);
     for(i=0;i<NUM;i++)
         scanf("%d",&arr2[i]);
 
     for(i=0;i<NUM;i++)
         arr3[i] = arr1[i] + arr2[i];
 
-    printf("\nSum array : ");
+    printf("Sum array : ");
     for(i=0;i<NUM;i++)
         printf("%d ",arr3[i]);
     
